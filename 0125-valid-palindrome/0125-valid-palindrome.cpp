0cda65cf65cc1b0ecfa043c09bdb3b9162891b02
Ayush gevariya <96529708+ayush8914@users.noreply.github.com>
2023-08-16 14:53:19 +0530

    Time: 9 ms (22.81%), Space: 7.4 MB (49.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        set<char> st;
-        for(int i=0;i<s.length();i++){
-            if(!isalnum(s[i]))st.insert(s[i]);
+        // set<char> st;
+        // for(int i=0;i<s.length();i++){
+        //     if(!isalnum(s[i]))st.insert(s[i]);
+        // }
+
+        // for(auto i : st){
+        //  s.erase(remove(
+        //         s.begin(), s.end(), i),
+        //     s.end());
+        // }
+
+        // transform(s.begin(), s.end(), s.begin(), ::tolower);
+        // string t=s;
+        // reverse(s.begin(),s.end());
+        // cout<<t<<"\n"<<s;
+        // if(t == s)return true;
+        // else
+        // return false;
+
+
+        int n= s.length();
+        int l=0;
+        int r=n-1;
+
+        while(l< r){
+            
+            if(isalnum(s[l]) && isalnum(s[r] ) && tolower(s[l]) == tolower(s[r])){
+                l++;
+                r--;
+            }
+            if(!isalnum(s[l])){
+                l++;
+            }
+            if(!isalnum(s[r])){
+                r--;
+            }
+            if(isalnum(s[l]) && isalnum(s[r] ) && tolower(s[l]) != tolower(s[r]))return false;
         }
 
-        for(auto i : st){
-         s.erase(remove(
-                s.begin(), s.end(), i),
-            s.end());
-        }
-
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        string t=s;
-        reverse(s.begin(),s.end());
-        cout<<t<<"\n"<<s;
-        if(t == s)return true;
-        else
-        return false;
+        return true;
     }
 };
